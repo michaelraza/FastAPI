3bfb8aeb36884316a5d32ce032dd2fca71dcc716
michaelraza <131973610+michaelraza@users.noreply.github.com>
2023-06-06 17:10:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, HTTPException, Response, status
 from typing import Optional, Union
 from pydantic import BaseModel
 
@@ -37,14 +37,22 @@ articles = [
         {"itemId": "item_id2", "itemName" : "item_name2", "itemPrice": "prices2"},
         {"itemId": "item_id3", "itemName" : "item_name3", "itemPrice": "prices3"} 
     ]         
-@app.get("/articles/{item_id}")  
-async def articles_items(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/articles/{itemId}")  
+async def get_item(itemId: int, response: Response):
+    try:
+        corresponding_product = articles[itemId - 1]
+        return corresponding_product
+    except:    
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND,
+            detail = "Article not found")
+
 
 @app.post("/articles")
-async def create_article(payload: Articles):
+async def create_article(payload: Articles, response: Response):
     print(payload.itemName)
     articles.append(payload.dict())
+    response.status_code = status.HTTP_201_CREATED
     return {
         "message": f"{payload.itemName} added successfully"
     }
